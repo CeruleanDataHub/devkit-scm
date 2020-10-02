4e34e59ead49356a288ae350c8322bb78b536841
Roope Pajunen <roope.pajunen@gmail.com>
2020-10-02 12:11:47 +0300

    Start using truffle develop network in deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ const idHex = '0x736f6d652d6964'
 const nameHex = '0x736f6d652d6e616d65'
 
 module.exports = function (deployer, network) {
-  if (network == 'development') {
+  if (network == 'development' || 'truffle_develop') {
     deployer.deploy(DevkitFactory).then(function () {
       return deployer.deploy(Devkit, idHex, nameHex, DevkitFactory.address)
     })
